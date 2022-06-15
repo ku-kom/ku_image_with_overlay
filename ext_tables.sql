763db2e47cf091b69f22395f4bfc119921551f58
nanna-dk <nanna@e-nanna.dk>
2022-06-15 14:37:46 +0200

    Overlay settings

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,5 +3,5 @@
 #
 # KU color family for images with overlay
 CREATE TABLE tt_content (
-    ku_color_family varchar(255) DEFAULT '' NOT NULL,
+    ku_color_family varchar(100) DEFAULT '' NOT NULL,
 );
