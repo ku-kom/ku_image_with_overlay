871b6e38f898b1a1fbe2ff863f59663060d8c7bc
nanna-dk <nanna@e-nanna.dk>
2022-06-16 08:22:07 +0200

    Setup

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,17 @@
 <?php
-defined('TYPO3') or die('Access denied.');
-/***************
- * Add default RTE configuration
+
+/*
+ * This file is part of the package ku_image_with_overlay.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
+
+defined('TYPO3') or die('Access denied.');
+// Add default RTE configuration
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ku_image_with_overlay'] = 'EXT:ku_image_with_overlay/Configuration/RTE/Default.yaml';
 
-/***************
- * PageTS
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ku_image_with_overlay/Configuration/TsConfig/Page/All.tsconfig">');
+// PageTS
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+    @import \'EXT:ku_image_with_overlay/Configuration/TsConfig/Page/All.tsconfig\'
+');
