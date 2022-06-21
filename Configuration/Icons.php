3a11b23832ef6ec834faa5996155beafab297ce4
nanna-dk <nanna@e-nanna.dk>
2022-06-21 09:30:59 +0200

    Icon registry

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -9,9 +9,7 @@ defined('TYPO3_MODE') || die();
    return [
        // icon identifier
        'ku-image-with-overlay-icon' => [
-            // icon provider class
            'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            // the source SVG for the SvgIconProvider
            'source' => 'EXT:ku_image_with_overlay/Resources/Public/Icons/Extension.svg',
        ],
    ];
