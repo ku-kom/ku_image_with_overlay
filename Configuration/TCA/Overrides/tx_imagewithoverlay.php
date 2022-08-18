556795c680d1228227b6eeaaa7b003448291e433
nanna-dk <nanna@e-nanna.dk>
2022-08-18 15:49:00 +0200

    Added backend preview

---
+++ Configuration/TCA/Overrides/tx_imagewithoverlay.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the package ku_image_with_overlay.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3_MODE') or die();
+
+/**
+ * Registers previewRenderer for custom content element "Image with overlay".
+ */
+
+$GLOBALS['TCA']['tt_content']['types']['ku_image_with_overlay']['previewRenderer'] = \UniversityOfCopenhagen\KuImageWithOverlay\Backend\Preview\imageWithOverlayPreviewRenderer::class;
